e01377fbb21cb24dc3ea5ea043602690c3cca31d
Danny <danny.v.der.pol@gmail.com>
2024-12-22 21:10:58 +0100

    add defines for folders

--- initialize.php
+++ initialize.php
@@ -13,15 +13,24 @@ spl_autoload_register("framework\\autoloader");
 // Initialize the session
 session_start();
 
-define("FRAMEWORK_FOLDER",      __DIR__);
-define("FRAMEWORK_LOG_FOLDER",  __DIR__ . "/.logs/");
 define("LOG_TIME_FORMAT",       "Y-m-d H:i:s");
 define("MAX_LOG_LINES",         1000);
+
+// Paths
+define("FRAMEWORK_FOLDER",      rtrim(str_replace("\\", "/", __DIR__), "\\/") . "/");
+define("FRAMEWORK_LOG_FOLDER",  FRAMEWORK_FOLDER . ".logs/");
+define("SERVER_ROOT",           rtrim($_SERVER["DOCUMENT_ROOT"], "\\/") . "/");
+define("WEB_FOLDER",            trim(dirname($_SERVER["PHP_SELF"]), "\\/") . "/");
+
+define("REQUEST_URI",           $_SERVER["REQUEST_URI"]);
 
 define("FRAMEWORK_LOG",         new framework\ModelLogger("framework"));
 
 FRAMEWORK_LOG->writeMessage("----------------------------------- Framework start -----------------------------------");
-FRAMEWORK_LOG->writeMessage("FRAMEWORK_FOLDER     : '" . FRAMEWORK_FOLDER . "'");
-FRAMEWORK_LOG->writeMessage("FRAMEWORK_LOG_FOLDER : '" . FRAMEWORK_LOG_FOLDER . "'");
 FRAMEWORK_LOG->writeMessage("LOG_TIME_FORMAT      : '" . LOG_TIME_FORMAT . "'");
 FRAMEWORK_LOG->writeMessage("MAX_LOG_LINES        : " . MAX_LOG_LINES);
+FRAMEWORK_LOG->writeMessage("FRAMEWORK_FOLDER     : '" . FRAMEWORK_FOLDER . "'");
+FRAMEWORK_LOG->writeMessage("FRAMEWORK_LOG_FOLDER : '" . FRAMEWORK_LOG_FOLDER . "'");
+FRAMEWORK_LOG->writeMessage("SERVER_ROOT          : '" . SERVER_ROOT . "'");
+FRAMEWORK_LOG->writeMessage("WEB_FOLDER           : '" . WEB_FOLDER . "'");
+FRAMEWORK_LOG->writeMessage("REQUEST_URI          : '" . REQUEST_URI . "'");
