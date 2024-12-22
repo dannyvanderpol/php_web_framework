e3a12423c9186ae0819a5696f22e6550926f484f
Danny <danny.v.der.pol@gmail.com>
2024-12-22 18:52:48 +0100

    add framework controller

---
+++ controllers/controllerFramework.php
@@ -0,0 +1,11 @@
+<?php namespace framework;
+
+// Handles the URI request and calls the user defined controller and action
+class ControllerFramework
+{
+    public static function getResponse()
+    {
+        $response = "TODO: parse URI and call controller and action";
+        return $response;
+    }
+}
