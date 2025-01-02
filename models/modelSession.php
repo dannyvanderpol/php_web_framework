404ab8ef82cd7b05831011caa871895331eb3b70
Danny <danny.v.der.pol@gmail.com>
2025-01-02 18:27:39 +0100

    add model for handling session

---
+++ models/modelSession.php
@@ -0,0 +1,41 @@
+<?php namespace framework;
+
+/* Model for handling sessions. */
+
+class ModelSession
+{
+    public static function setData($key, $data)
+    {
+        $_SESSION[$key] = $data;
+    }
+
+    public static function getData($key, $default=null)
+    {
+        return arrayGey($_SESSION, $key, $default);
+    }
+
+    public static function clearData($key)
+    {
+        if (isset($_SESSION[$key]))
+        {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    public static function destroySession()
+    {
+        // Clear all cookies for this session
+        if (ini_get("session.use_cookies"))
+        {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+        session_destroy();
+        session_start();
+    }
+}
+
+?>
